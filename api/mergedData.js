8e979ee3c472bec3206e20f133e043aa09a4b0eb
Andrew <andrewspurlock@rocketmail.com>
2024-02-22 20:46:27 -0600

    completed edit and details for books and authors

--- api/mergedData.js
+++ api/mergedData.js
@@ -1,1 +1,22 @@
 // for merged promises
+
+import { getAuthorBooks, getSingleAuthor } from './authorData';
+import { getSingleBook } from './bookData';
+
+// TODO: Get data for viewBook
+const getBookDetails = async (firebaseKey) => {
+  const bookObject = await getSingleBook(firebaseKey);
+  const authorObject = await getSingleAuthor(bookObject.author_id);
+
+  return { ...bookObject, authorObject };
+};
+
+const getAuthorDetails = async (firebaseKey) => {
+  console.warn(firebaseKey);
+  const authorObject = await getSingleAuthor(firebaseKey);
+  const authorBooks = await getAuthorBooks(firebaseKey);
+  console.warn(authorBooks);
+  return { ...authorObject, books: authorBooks };
+};
+
+export { getBookDetails, getAuthorDetails };
